0922ac3357ff00333e83fae02b96b1e7ba86112b
Rafael Noll da Silva <rafael.silva@pubgenius.io>
2026-05-28 09:31:35 -0300

    Adding gateway service

---
+++ gateway-service/src/main/java/br/edu/atitus/gatewayservice/configs/GatewayConfig.java
@@ -0,0 +1,18 @@
+package br.edu.atitus.gatewayservice.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    RouteLocator getGatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/get").uri("http://httpbin.org"))
+                .route(p -> p.path("/products/**").uri("lb://product-service"))
+                .route(p -> p.path("/currency/**").uri("lb://currency-service"))
+                .build();
+    }
+}
